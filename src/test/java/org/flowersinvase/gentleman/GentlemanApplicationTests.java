4212d6b7da08e07d9ebb405d58495483b748ca44
Sergey Meshcheryakov <vazko163@gmail.com>
2026-08-13 08:51:10 +0400

    Initial commit

---
+++ src/test/java/org/flowersinvase/gentleman/GentlemanApplicationTests.java
@@ -0,0 +1,13 @@
+package org.flowersinvase.gentleman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GentlemanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
